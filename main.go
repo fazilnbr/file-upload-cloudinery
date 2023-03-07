c2048c11086bca920df53c011cc31a44af3fe792
fazil <fazil@pop-os.localdomain>
2023-03-08 04:23:21 +0530

    swagger anotation
    headder annotation added in main.go

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-    "file-upload-cloudinary/controllers" //add this
-    "github.com/gin-gonic/gin"
+	"file-upload-cloudinary/controllers" //add this
+
+	"github.com/gin-gonic/gin"
 )
 
+// @title Go + Gin File Upload API
+// @version 1.0
+// @description This is a sample server file upload server. You can visit the GitHub repository at https://github.com/fazilnbr/file-upload-cloudinery
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email fazilkp2000@gmail.com
+
+// @license.name MIT
+// @license.url https://opensource.org/licenses/MIT
+
+// @host localhost:6000
+// @BasePath /
+// @query.collection.format multi
+
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    //add
-    router.POST("/file", controllers.FileUpload())
-    router.POST("/remote", controllers.RemoteUpload())
+	//add
+	router.POST("/file", controllers.FileUpload())
+	router.POST("/remote", controllers.RemoteUpload())
 
-    router.Run("localhost:6000")
+	router.Run("localhost:6000")
 }
