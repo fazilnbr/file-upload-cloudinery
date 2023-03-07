3902e9ae1ee5517e67b407009a6bbf01c6e59661
fazil <fazil@pop-os.localdomain>
2023-03-08 03:45:55 +0530

    add functons
    here add all essential functions

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+    "file-upload-cloudinary/controllers" //add this
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    router := gin.Default()
+
+    //add
+    router.POST("/file", controllers.FileUpload())
+    router.POST("/remote", controllers.RemoteUpload())
+
+    router.Run("localhost:6000")
+}
---
+++ services/media_service.go
@@ -0,0 +1,55 @@
+package services
+
+import (
+	"file-upload-cloudinary/helper"
+	"file-upload-cloudinary/models"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var (
+	validate = validator.New()
+)
+
+type mediaUpload interface {
+	FileUpload(file models.File) (string, error)
+	RemoteUpload(url models.Url) (string, error)
+}
+
+type media struct{}
+
+func NewMediaUpload() mediaUpload {
+	return &media{}
+}
+
+func (*media) FileUpload(file models.File) (string, error) {
+	//validate
+	err := validate.Struct(file)
+	if err != nil {
+		return "", err
+	}
+
+	//upload
+	uploadUrl, err := helper.ImageUploadHelper(file.File)
+	fmt.Printf("\nerror : %v\n", err)
+	if err != nil {
+		return "", err
+	}
+	return uploadUrl, nil
+}
+
+func (*media) RemoteUpload(url models.Url) (string, error) {
+	//validate
+	err := validate.Struct(url)
+	if err != nil {
+		return "", err
+	}
+
+	//upload
+	uploadUrl, errUrl := helper.ImageUploadHelper(url.Url)
+	if errUrl != nil {
+		return "", err
+	}
+	return uploadUrl, nil
+}
